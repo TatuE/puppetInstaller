f04b67961d1be3f0ad2f9620da0a5eddc9125ae6
TatuE <tatu.erkinjuntti@gmail.com>
2017-11-03 23:15:14 +0200

    Add files via upload

---
+++ puppetInstaller.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+
+sudo apt-get install -y puppet git
+
+git clone https://github.com/TatuE/puppet.git
+
+cd puppet/
+
+sudo cp -r apache/ mysql/ ssh/ ufw/ /etc/puppet/modules/
+sudo cp site.pp /etc/puppet/manifests/
+
+sudo puppet apply /etc/puppet/manifests/site.pp
+
+echo " "
+echo "Install complete!"
+echo " "
