9ee6b9c974c2bbfc3fc94a2df7851a16dab559f7
TatuE <tatu.erkinjuntti@gmail.com>
2017-11-05 23:56:16 +0200

    updated modulepaths

--- puppetSlaver.sh
+++ puppetSlaver.sh
@@ -7,6 +7,8 @@ git clone https://github.com/TatuE/puppetSlaver.git
 
 sudo cp -r puppetSlaver /etc/puppet/modules/
 
+cd /etc/puppet/
+
 puppet apply --modulepath modules/ -e 'class {"puppetSlaver":}'
 
 
