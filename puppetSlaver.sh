f85045dba440cb4af758c570a9c6714cc955f268
TatuE <tatu.erkinjuntti@gmail.com>
2017-11-05 23:28:29 +0200

    Script to create a puppet slave

---
+++ puppetSlaver.sh
@@ -0,0 +1,15 @@
+
+sudo apt-get update
+
+sudo apt-get install -y puppet git
+
+git clone https://github.com/TatuE/puppetSlaver.git
+
+sudo cp -r puppetSlaver /etc/puppet/modules/
+
+puppet apply --modulepath modules/ -e 'class {"puppetSlaver":}'
+
+
+echo " "
+echo "Slaving complete!"
+echo " "
